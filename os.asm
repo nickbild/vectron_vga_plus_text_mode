48a83ea226f2da84c23f369b63d402732c67b671
Nick Bild <nick.bild@gmail.com>
2021-12-22 17:16:38 -0600

    Save progress on ISR.

--- os.asm
+++ os.asm
@@ -8,16 +8,16 @@
 ; Reserved memory:
 ;
 ; $0000-$7EFF - RAM
-; 		$0000-$0003 - Named variables
+; 		$0000-$000A - Named variables
 ; 		$0100-$01FF - 6502 stack
 ;		$7FF0-$7FFF - 16-bit Decoder
 ;			$7FF0 - Data input: Row
 ;			$7FF1 - Data input: Column
-;			$7FF2 - Data input: Value
+;			$7FF2 - Data input: RAM Value
 ;			$7FF3 - Data output: Address 0-7
 ;			$7FF4 - Data output: Address 8-15
 ;			$7FF5 - Data output: Address 16-17
-;			$7FF6 - Data output: Value
+;			$7FF6 - Data output: RAM Value
 ;			$7FF7 - Data output: WE/CE
 ; $8000-$FFFF - ROM
 ; 		$FFFA-$FFFB - NMI IRQ Vector
@@ -36,6 +36,25 @@ addrMid
 addrHigh
 		.byte #$00
 data
+		.byte #$00
+
+num1Low
+		.byte #$00
+num1Mid
+		.byte #$00
+num1High
+		.byte #$00
+num2Low
+		.byte #$00
+num2Mid
+		.byte #$00
+num2High
+		.byte #$00
+result2Low
+		.byte #$00
+result2Mid
+		.byte #$00
+result2High
 		.byte #$00
 
 
@@ -324,14 +343,166 @@ WriteData
 	rts
 
 
+Add24BitNumbers
+	clc
+	lda num1Low
+	adc num2Low
+	sta num1Low
+	
+	lda num1Mid
+	adc num2Mid
+	sta num1Mid
+
+	lda num1High
+	adc num2High
+	sta num1High
+
+	rts
+
+
+NextAddressRow
+	; Add 392 to address.
+	clc
+	lda addrLow
+	adc #$88
+	sta addrLow
+	
+	lda addrMid
+	adc #$01
+	sta addrMid
+
+	lda addrHigh
+	adc #$00
+	sta addrHigh
+
+	rts
+
+
+DrawCharacterLine
+	.byte #$DA ; phx - mnemonic unknown to DASM.
+
+	ldx #$08
+DrawCharacterLine1
+	; LDA #$08
+	; BIT FLAGS
+	; BNE Bit3IsSet
+	; BEQ Bit3isClear
+
+	; TODO: set data for each pixel.
+	lda #$1F
+	sta data
+
+	jsr WriteData
+	dex
+	bne DrawCharacterLine1
+	jsr NextAddressRow
+
+	.byte #$FA ; plx
+
+	rts
+
+
 DrawTextIsr	ORG $8500
-	; lda $7FF0
-    ; sta $7FF6
+	; Convert row and column into starting memory address.
+	; (row * 400) + col
+	; ((row * 16) * 400) + (col * 8)
 
-	; lda $7FF1
-    ; sta $7FF6
+	; Zero the numbers to add.
+	lda #$00
+	sta num1Low
+	sta num1Mid
+	sta num1High
+	sta num2Low
+	sta num2Mid
+	sta num2High
 
-	; lda $7FF2
-    ; sta $7FF6
+	; Load row value to memory, multiply by 16.
+	lda $7FF0
+	sta num2Low
+	
+	ldx #$10
+DrawTextIsr1
+	jsr Add24BitNumbers
+	dex
+	bne DrawTextIsr1
+
+	; Multiply the above value by 400.
+	lda num1Low
+	sta num2Low
+	lda num1Mid
+	sta num2Mid
+	lda num1High
+	sta num2High
+
+	lda #$00
+	sta num1Low
+	sta num1Mid
+	sta num1High
+
+	ldx #$FF
+DrawTextIsr2
+	jsr Add24BitNumbers
+	dex
+	bne DrawTextIsr2
+
+	ldx #$91
+DrawTextIsr3
+	jsr Add24BitNumbers
+	dex
+	bne DrawTextIsr3
+
+	; Save result thus far.
+	lda num1Low
+	sta resultLow
+	lda num1Mid
+	sta resultMid
+	lda num1High
+	sta resultHigh
+
+	; Add the column value to memory.
+	
+	; Zero the numbers to add.
+	lda #$00
+	sta num1Low
+	sta num1Mid
+	sta num1High
+	sta num2Low
+	sta num2Mid
+	sta num2High
+	
+	; Load the column value into memory.
+	lda $7FF1
+	sta num2Low
+
+	; Multiply column value by 8.
+	ldx #$08
+DrawTextIsr4
+	jsr Add24BitNumbers
+	dex
+	bne DrawTextIsr4
+
+	; Add column offset to previously saved value.
+	lda resultLow
+	sta num2Low
+	lda resultMid
+	sta num2Mid
+	lda resultHigh
+	sta num2High
+
+	jsr Add24BitNumbers	; After this point, the starting address is stored in num1...
+
+	lda num1Low
+	sta addrLow
+	lda num1Mid
+	sta addrMid
+	lda num1High
+	sta addrHigh
+	
+	; Now draw the character at this position.
+	ldx #$10
+DrawTextIsr5
+	jsr DrawCharacterLine
+	dex
+	bne DrawTextIsr5
 
 	rti
